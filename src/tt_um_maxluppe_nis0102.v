26d4626a41a14e30cebb87acf8be1b7a356ff710
Maximiliam Luppe <maxluppe@users.noreply.github.com>
2024-04-24 14:52:28 -0300

    Update and rename project.v to tt_um_maxluppe_nis0102.v

--- src/tt_um_maxluppe_nis0102.v
+++ src/tt_um_maxluppe_nis0102.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Maximiliam Luppe
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_maxluppe_nis0102 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,8 +17,18 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+assign uo_out[7:4]  = 0;
+assign uio_out = 0;
+assign uio_oe  = 0;
+
+NIST0102 u0 (
+    .clk(clk),
+    .rst_n(rst_n),
+    .RND_in(ui_in[0]),
+    .nPass1(uo_out[0]),
+    .nPass2(uo_out[1]),
+    .nBlkOK1(uo_out[2]),
+    .nBlkOK2(uo_out[3])
+);
 
 endmodule
